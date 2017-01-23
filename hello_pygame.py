0ad5802094462a4ae0e810008c758cb1726434ca
unknown <>
2017-01-23 15:08:26 +0800

    add gameover

--- hello_pygame.py
+++ hello_pygame.py
@@ -3,6 +3,27 @@ from sys import exit
 import time
 import random
 
+class Aircraft:
+    def restart(self):
+        self.x = 1000
+        self.y = 800
+
+    def __init__(self):
+        self.restart()
+        self.image = pygame.image.load('ufo.png').convert_alpha()
+
+    def move(self):
+        x, y = pygame.mouse.get_pos()
+        #turned out aircraft(x,y) actually is aircraft's top left corner(x,y)
+        #to make mouse at the center of aircraft,move more
+        #move towards origin flatly -> (0,height/2)<-relatively to mouse
+        x -= self.image.get_width() / 2
+        #up -> (0,0)
+        y -= self.image.get_height() / 2
+        #reassign examp's attribute
+        self.x = x
+        self.y = y
+
 class Bullet:
     #initialize bullet's position and variable to save
     #set initial y<0 then it will run path1 in later loop
@@ -35,11 +56,11 @@ class Enemy:
     def restart(self):
         self.x = random.randint(100,750)
         self.y = random.randint(-200,-50)
-        #can I directly use speed??
         self.speed = random.random() + 0.1
 
     def __init__(self):
         self.restart()
+        #to enrich game I use choice to add diverse enemy,turn out they don't always all appear in a round:(WHAT SHOULD I DO?
         self.image = pygame.image.load(random.choice(['red.png','yellow.png','boss.png'])).convert_alpha()
 
     def move(self):
@@ -57,6 +78,12 @@ def checkhit(bullet,enemy):
     else:
         pass
 
+def checkcrash(aircraft,enemy):
+    if (aircraft.x + 0.5*aircraft.image.get_width() > enemy.x) and (aircraft.x + 0.5*aircraft.image.get_width() < enemy.x + enemy.image.get_width()) and (aircraft.y + 0.5*aircraft.image.get_height() > enemy.y) and (aircraft.y + 0.5*aircraft.image.get_height() < enemy.y +enemy.image.get_height()):
+        return True
+    else:
+        return False
+
 pygame.init()
 
 #open game window
@@ -65,6 +92,8 @@ pygame.display.set_caption("Stardust")
 #pull in images
 background = pygame.image.load('back.jpg').convert()
 aircraft = pygame.image.load('ufo.png').convert_alpha()
+
+aircraft = Aircraft()
 
 #create bullets' box
 bullets = []
@@ -84,6 +113,8 @@ enemies = []
 for i in range(3):
     enemies.append(Enemy())
 
+gameover = False
+
 #the main loop of game
 while True:
     #respond to event that user trigger 
@@ -96,35 +127,36 @@ while True:
     #draw background on canvas
     screen.blit(background, (0,0))
 
-    #like countdown:3,2,1,shoot!
-    interval_b -= 1
-    if interval_b < 0:
-        bullets[index_b].restart()
-        #reset for countdowm again
-        interval_b = 100
-        #choose the next bullet
-        #0%5=0, 3%5=3, 5%5=0, smart loop!
-        index_b = (index_b + 1) % count_b
-    #give bullet method according to its status
-    for b in bullets:
-        if b.active == True:
-            #check each bullet whether it hit an enemy
-            for e in enemies:
-                checkhit(b,e)
-            b.move()
-            #draw bullet before aircraft then the bullet fly from under it
-            screen.blit(b.image, (b.x, b.y))
+    if gameover == False:
+
+        #like countdown:3,2,1,shoot!
+        interval_b -= 1
+        if interval_b < 0:
+            bullets[index_b].restart()
+            #reset for countdowm again
+            interval_b = 100
+            #choose the next bullet
+            #0%5=0, 3%5=3, 5%5=0, smart loop!
+            index_b = (index_b + 1) % count_b
+        #give bullet method according to its status
+        for b in bullets:
+            if b.active == True:
+                #check each bullet whether it hit an enemy
+                for e in enemies:
+                    checkhit(b,e)
+                b.move()
+                #draw bullet before aircraft then the bullet fly from under it
+                screen.blit(b.image, (b.x, b.y))
             
-    for e in enemies:
-        e.move()
-        screen.blit(e.image, (e.x, e.y))
-    #if directly use x,y the aircraft will be at the lower right corner of mouse.Why?...
-    x, y = pygame.mouse.get_pos()
-    #to make mouse at the center of aircraft,move aircraft more
-    #move towards origin flatly -> (0,height/2)<-relatively to mouse
-    x -= aircraft.get_width() / 2
-    #up -> (0,0)
-    y -= aircraft.get_height() / 2
-    #now aircraft's(x,y) = mouse's(x,y),draw aircraft!
-    screen.blit(aircraft, (x,y))
-    pygame.display.update()
+        for e in enemies:
+            if checkcrash(aircraft,e) == True:
+                gameover = True
+            e.move()
+            screen.blit(e.image, (e.x, e.y))
+
+        aircraft.move()
+        screen.blit(aircraft.image, (aircraft.x,aircraft.y))
+        pygame.display.update()
+
+    else:
+        pass
