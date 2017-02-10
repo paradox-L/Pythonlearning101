95c4788bd9235bd13a852f5485514cb67278cd62
unknown <>
2017-02-10 17:35:35 +0800

    web app finished

--- basictodo/schema.sql
+++
@@ -1,5 +0,0 @@
-create table todo (
-	id int auto_increment,
-	title text
-	primary key(id)
-)
--- todo/todo.sql
+++ todo/todo.sql
@@ -1,7 +1,9 @@
 create table todoitem(id INTEGER PRIMARY KEY, content TEXT, itemdate DATE);
 
-insert into todoitem values (0, 'Demo is an example', '1970-01-01');
+insert into todoitem values (0,'Demo is an example','1970-01-01');
     
-insert into todoitem values (1, 'Delete item in detail page by clicking it', '1988-01-01');
+insert into todoitem values (1,'Delete item in detail page by clicking it','1988-01-01');
 
-insert into todoitem values (2, 'Have a good time!', '2013-01-01');
+insert into todoitem values (2,'Have a good time!','2013-01-01');
+
+insert into todoitem values (3,'据测试中文输入也可以。然而用python写东西我一直比较担心乱码，编码声明都没写0 0You can try!','2017-02-10');
